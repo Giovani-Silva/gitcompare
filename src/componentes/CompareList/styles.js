c427cbc7744cc94777a6656075ed5fe3ed4390bb
Giovani Silva <giovani.silva@live.com>
2018-12-10 15:06:28 -0200

    Estilizando lista de repos

---
+++ src/componentes/CompareList/styles.js
@@ -0,0 +1,53 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  margin-top: 50px;
+`;
+
+export const Repository = styled.div`
+  width: 250px;
+  background: #fff;
+  border-radius: 3px;
+  margin: 0 10px;
+  display: flex;
+  flex-direction: column;
+  box-shadow: 0px 2px 15px -2px rgba(0, 0, 0, 0.6);
+
+  header {
+    padding: 30px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    img {
+      width: 64px;
+    }
+    strong {
+      font-size: 2rem;
+      margin-top: 10px;
+    }
+    small {
+      font-size: 1.2rem;
+      color: #999;
+    }
+  }
+  ul {
+    list-style: none;
+    li {
+      font-weight: bold;
+      padding: 12px 20px;
+      small {
+        font-weight: normal;
+        font-size: 1rem;
+        color: #999;
+        font-style: italic;
+        margin-left: 5px;
+      }
+      &:nth-child(2n-1) {
+        background: #f5f5f5;
+      }
+    }
+  }
+`;
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,16 +1,13 @@
-import React from 'react';
 import styled from 'styled-components';
 
-import logo from '../assets/logo.png';
-
-const Container = styled.div`
+export const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   padding-top: 60px;
 `;
 
-const Form = styled.div`
+export const Form = styled.div`
   margin-top: 20px;
   width: 100%;
   max-width: 400px;
@@ -40,16 +37,3 @@ const Form = styled.div`
     }
   }
 `;
-
-const Main = () => (
-  <Container>
-    <img src={logo} alt="Github Compare" />
-
-    <Form>
-      <input type="text" placeholder="usuário/repositório" />
-      <button type="submit">OK</button>
-    </Form>
-  </Container>
-);
-
-export default Main;
